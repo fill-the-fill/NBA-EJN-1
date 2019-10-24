055981d00decbd453c91c70d5862b23794820e24
JonaCodes <jonathanfarache@gmail.com>
2019-10-24 15:25:35 +0300

    remove body parser & whitespace

--- server.js
+++ server.js
@@ -5,23 +5,12 @@ const bodyParser = require('body-parser')
 const api = require('./server/routes/api')
 const app = express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 
-
 app.use('/', api)
-
-
 
 const port = 3000
 app.listen(port, function () {
     console.log(`Running server on port ${port}`)
 })    
-
-
-
-
-
-
