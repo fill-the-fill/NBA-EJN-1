4a3e6b6f5672ec1f10ab16095e3b045fd1ea6ea5
danbrud <dannybrudner@users.noreply.github.com>
2019-12-16 17:42:35 +0200

    added default image for broken links

--- dist/main.js
+++ dist/main.js
@@ -1,16 +1,19 @@
 const source = $(`#roster-template`).html()
 const template = Handlebars.compile(source)
 
-const render = function(roster){
+const render = function (roster) {
     $(`#roster`).empty()
-    let newHTML = template({player: roster})
+    let newHTML = template({ player: roster })
     $(`#roster`).append(newHTML)
 }
 
-const fetch = function(){
+const fetch = function () {
     let input = $(`#input`).val()
-$.get(`/teams/${input}`, function(response){
-    render(response)
-})
+    $.get(`/teams/${input}`, function (response) {
+        render(response)
+    })
 }
 
+const setDefaultImg = function (img) {
+    img.src = 'https://p7.hiclipart.com/preview/729/435/787/basketball-sport-silhouette-clip-art-nba-players.jpg'
+}
--- server/routes/api.js
+++ server/routes/api.js
@@ -13,18 +13,19 @@ const json = {
     data: {}
 }
 
+
+
 request('http://data.nba.net/10s/prod/v1/2018/players.json', (err, res) => {
     json.data = JSON.parse(res.body).league.standard
 })
 
 router.get(`/teams/:teamName`, (req, res) => {
     let teamID = teamToIDs[req.params.teamName]
-    console.log(req.params.teamName)
+
     let team = json.data
         .filter(d => d.teamId === teamID && d.isActive)
         .map(d => { return { firstName: d.firstName, lastName: d.lastName, jersey: d.jersey, pos: d.pos } })
-        .filter(d => !["5", "00", "0", "55", "32", "10"].includes(d.jersey))
-    console.log(team)
+
     res.send(team)
 })
 
